2e6567fd3954e6e5fab140487fb215e4a5bee0f2
cccampb2 <calebccampbell19@gmail.com>
2025-05-08 18:39:32 -0400

    feat(header): design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,28 @@
 import "./Header.css";
 
-function Header() {
-  return <></>;
+function Header({ isLoggedIn }) {
+  return (
+    <header className="header">
+      <div className="header__items">
+        <div className="header__title">NewsExplorer</div>
+        <nav className="header__navbar">
+          <ul className="nav__items">
+            <li className="nav__item">
+              <a className="nav__link" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#saved">
+                Saved Articles
+              </a>
+            </li>
+          </ul>
+          <button className="nav__sign-in-btn">Sign In</button>
+        </nav>
+      </div>
+    </header>
+  );
 }
+
+export default Header;
